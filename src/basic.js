ba60c972833f7cb31b42619be14543585cd33400
shenpengfei <shenpengfei@didiglobal.com>
2019-04-17 14:03:57 +0800

    fix: 增加basic中答案

--- src/basic.js
+++ src/basic.js
@@ -109,9 +109,8 @@ typeof []; // 判断数组的方法1 2
  * 12 常用的图片的格式  webp
  *
  * */
-* 1. 写出运行的结果
-*
-*/
+// 1. 写出运行的结果
+
  [] === 0; //false
  [] == 0;  //"" 0 true
  null == 0; //false
@@ -134,42 +133,45 @@ typeof []; //"object"
 Array.isArray([])
 Object.prototype.toString().apply([]) === "[object Array]"
 Object.prototype.toString().call([]) === '[object Array]'
+// 2.js 中定义变量的方式有几种
+//var let function class import Symbol
+//*2.1 函数声明和函数表达式的区别
+/* 函数声明 function funName(){}--声明提前
+函数表达式 var funName = function(){}-执行到此行才创建*/
+// var a = b = 1; // 出现的问题是什么
 /*
-* 2.js 中定义变量的方式有几种
-* var let function class import Symbol
-* 2.1 函数声明和函数表达式的区别
-* 函数声明 function funName(){}--声明提前
-函数表达式 var funName = function(){}-执行到此行才创建
-* var a = b = 1; // 出现的问题是什么
-*同时赋值给一个引用类型，a和b引用关系相同
-* b 会成为全局变量
-* 3. typeof x  有多少种结果
-* "boolean" "undefined" "string" "number" "object" "function"
-* 3.1 parseInt parseFloat Number 区别
-*parseInt可以指定两个参数，第二个参数是进制 parseFloat()只能解析十进制
-* Number() false true '1px'
-* 3.2 isNaN()  // 存在的问题
-* isNaN() false, true > false
-*
-* 4.实现一个数组的乱序排列
-*var arr;
-arr.sort(randomSort())
-randomSort(val1,val2)
-{
-  var n = Math.random(0,arr.length)+1;
- if(n/2 === 0)
- {
-   return -1;
- }
- else{
-   return 1;
- }
-}
-// arr.sort((a, b) => Math.random() > 0.5)
-* 5.实现数组的排序
-* sort方法
+  同时赋值给一个引用类型，a和b引用关系相同
+  b 会成为全局变量 
+*/
+// 3. typeof x  有多少种结果
+//"boolean" "undefined" "string" "number" "object" "function" + symbol
+// 3.1 parseInt parseFloat Number 区别
+/*
+  parseInt可以指定两个参数，第二个参数是进制 parseFloat()只能解析十进制
+  Number() false true '1px'
+*/
+// 3.2 isNaN()  // 存在的问题
+// isNaN() false, true > false + 会使用Number进行类型转换
+
+//4.实现一个数组的乱序排列
+
 var arr;
-Array.property.method = function(f,index,arr)
+arr.sort(randomSort()) // + arr.sort((a, b) => (Math.random() > 0.5))
+function randomSort(val1,val2)
+{
+  var n = Math.random(0,arr.length)+1; // + Math.random()* arr.length + 1
+  if(n/2 === 0)
+  {
+    return -1;
+  }
+  else{
+    return 1;
+  }
+}
+// 5.实现数组的排序
+//  sort方法
+var arr;
+Array.property.method = function(f,index,arr) // 什么意思
 {
    for(var i = 0; i<arr.length;i++)
    {
@@ -178,8 +180,8 @@ Array.property.method = function(f,index,arr)
 }
 
 
-* 5.1 数组去重
-*  for(var i = 0 ; i< arr.length; i++)
+// 5.1 数组去重
+for(var i = 0 ; i< arr.length; i++)
 {
   for(var j = i+1; j<arr.length; j++)
   {
@@ -200,7 +202,6 @@ for(var i = 0;i <arr.length; i++ )
   }
 
 }
-console.log(Res);
 // 对象的属性 [1,2,3,'1',1]
 function unique (arr) {
   let obj = {}
@@ -217,10 +218,10 @@ function unique (arr) {
 // set
 function unique(arr) {
   let set = new Set(arr)
-  //return Array.from(set)
-  // return [...set]
+  // return Array.from(set)
+  return [...set]
 }
-//reduce
+//reduce 
 var Res = function(arr)
 {
   return arr.reduce(function(prev,cur,index,arr)
@@ -234,11 +235,10 @@ var Res = function(arr)
 
   },[]);
 }
-* 6.实现两个数组的交集、并集
-*
+// 6.实现两个数组的交集、并集
  var arr1, var arr2 ;
  var Res = [];
- 并集：
+ //并集：
  for(var i = 0;i < arr1.length;i++){
    if(Res.indexOf(arr1[i] === -1))
    {
@@ -254,11 +254,11 @@ var Res = function(arr)
 
    }
  }
-或者：
-Res = arr1.cocat(arr2);
-Res去重
-交集：
-1.
+//或者：
+Res = arr1.concat(arr2);
+//Res去重
+//交集：
+//交集1
 var ResJ = [];
 for(var i = 0;i<arr1.length;i++)
 {
@@ -274,6 +274,7 @@ for(var i = 0;i<arr1.length;i++)
     }
   }
 }
+// 交集2
 for(var i = 0;i<arr1.length;i++)
 {
   if(arr2.indexOf(arr1[i]) !== -1 && ResJ.indexOf(arr1[i]) === -1)
@@ -281,8 +282,8 @@ for(var i = 0;i<arr1.length;i++)
    ResJ.push(arr1[i]);
   }
 }
-* 7.实现数组的reduce方法 5 stars
-*  Array.property.reduce = function(f,value)
+//7.实现数组的reduce方法  5 stars
+Array.property.reduce = function(f,value)
 {
    for(var i = 0 ; i < this.length; i++)
    {
@@ -290,27 +291,23 @@ for(var i = 0;i<arr1.length;i++)
    }
    return value;
 }
-* 7.0 实现多层数组的展开
-* var arr = [
+//7.0 实现多层数组的展开
+var arr = [
   [1,2,3],
   [3,[4,5,6]]
 ];
 //reduce
-var Res = arr.reduce(function(prev,cur,index,arr)
-{
-    if()
-});
 var Res = function(arr)
 {
   return arr.reduce(function(prev,cur,index,arr)
   {
-    return prev.concat(Array.isArray(cur)?Res(cur):cur)
-  });
+    return prev.concat(Array.isArray(cur) ? Res(cur) : cur)
+  },[])
 }
-ES6
+//ES6
 var newArr = functing(arr)
 {
-   return arr.reduce((prev,cur)=>prev.concat(Array.isArray(next)?newArr(next):next))
+   return arr.reduce((prev,cur) => prev.concat(Array.isArray(cur) ? newArr(cur) : cur), [])
 }
 var Res = [];
 function openArray(arr)
@@ -330,20 +327,17 @@ function openArray(arr)
   }
   return aTmp;
 }
-// reduce
-
-* 7.1 深拷贝
-*
-// var arr;
-//var res = [];
-function deepclone(arr)
+// 7.1 深拷贝
+var arr;
+var res = [];
+function deepclone(arr) // 不对
 {
   var res = [];
   if(Array.isArray(arr))
   {
-     res = res.concat(arr.map(deepclone(arr)));
+     res = res.concat(arr.map(deepclone(arr))) // + res.concat(arr.map(deepclone))
   }
-  else if(Object.prototype.toString.apply(arr) === "[object object]")
+  else if(Object.prototype.toString.apply(arr) === "[object object]") // + [object Object]
   {
     for(var key in arr)
     {
@@ -360,8 +354,7 @@ function deepclone(arr)
 
 }
 
-* 7.2 判断两个对象是否相同
-*
+// 7.2 判断两个对象是否相同
 var obj1 = {name:'lilei',age:18,fruit:['apple','banna']};
 var obj2 = {name:'lilei',age:18,fruit:['apple','banna']};
 function equal(obj1,obj2)
@@ -387,16 +380,21 @@ function equal(obj1,obj2)
       }
       else
       {
-        if(typef(val1) == 'object')
+        if(typef(val1) === 'object' && va1 !== null)
         {
           return equal(val1,val2);
+        }  else {
+          if (val1 !== val2) { // +
+            return false
+          }
         }
       }
+      return true // +
 
     }
     allkeys.map(function(item,index,array)
     {
-      if(Object.keys.indexOf(item)== -1)
+      if(Object.keys.indexOf(item)== -1) // Object.keys(obj2).indexOf(item) === -1 
       {
         return false;
 
@@ -408,52 +406,51 @@ function equal(obj1,obj2)
 
 }
 
-* 7.3 Object.create()
-*
-* 7.4 for...in Object.keys的区别 判断属于自身属性的方法
-*for in包括原型中的属性，
-* 8. ../a/b/c  ../a/c 相对路径表示出来
-*
-* 9. 闭包 作用域链
-*
-* 9.1 this arguments
-*
-* 10. jsonp原理(手写jsonp) websocket  跨域的其他方法 axios preflight withcredients
-*
-* 10.1 fetch xhr
-*
-* 11. http 的状态码
-*200-OK 302-资源重定向 404-找不到 500-服务器错误
-* 11.1 输入url 到页面渲染出来的过程
-*dns解析 返回静态资源文件
+// 7.3 Object.create()
+
+// 7.4 for...in Object.keys的区别 判断属于自身属性的方法
+// for in包括原型中的属性，
+// 8. ../a/b/c  ../a/c 相对路径表示出来
+
+// 9. 闭包 作用域链
+//
+// 9.1 this arguments
+
+// 10. jsonp原理(手写jsonp) websocket  跨域的其他方法 axios preflight withcredients
+
+// 10.1 fetch xhr
+//
+// 11. http 的状态码
+//200-OK 302-资源重定向 404-找不到 500-服务器错误
+// 11.1 输入url 到页面渲染出来的过程
+/*dns解析 返回静态资源文件
 * css js 阻塞html 渲染 defer async type=module
-*
-* 11.2 首屏加载的优化
-*
-* 12 常用的图片的格式  webp
-* jpg gif png
+*/
+//11.2 首屏加载的优化
 
-* 13. js垃圾回收机制  标记清除 引用计数
-*
-* 14. requestAnimationFrame
-*
-* */
+// 12 常用的图片的格式  webp
+//jpg gif png
+
+// 13. js垃圾回收机制  标记清除 引用计数
+
+//14. requestAnimationFrame
+
 //一个作用域问题
-// function sendrequest()
-// {
-//   function onSucess()
-//   {
-//      console.log(sFileName);
-//   }
-//   var sFileName = "kkk.pdf";
+function sendrequest()
+{
+  function onSucess()
+  {
+     console.log(sFileName);
+  }
+  var sFileName = "kkk.pdf";
 
-//   Utis.Request.action([],
-//     {
-//       onSucess:onSucess,
-//       onSucess:function()
-//       {
-//         onSucess(sFileName);
-//       },
-//       onFailed: onFailed
-//     });
-// }
+  Utis.Request.action([],
+    {
+      onSucess:onSucess,
+      onSucess:function()
+      {
+        onSucess(sFileName);
+      },
+      onFailed: onFailed
+    });
+}
